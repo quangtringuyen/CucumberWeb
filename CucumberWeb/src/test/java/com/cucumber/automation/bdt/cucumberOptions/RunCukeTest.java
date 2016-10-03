dcede062bdb4d8a405963a477745a9fb68cbfca3
Tri Nguyen <quangtringuyen@outlook.com>
2016-10-03 23:54:27 +0700

    commit

--- CucumberWeb/src/test/java/com/cucumber/automation/bdt/cucumberOptions/RunCukeTest.java
+++ CucumberWeb/src/test/java/com/cucumber/automation/bdt/cucumberOptions/RunCukeTest.java
@@ -1,4 +1,11 @@
 package com.cucumber.automation.bdt.cucumberOptions;
+import cucumber.api.junit.*;
+import org.junit.runner.RunWith;
+
+@RunWith (Cucumber.class)
+@Cucumber.Options(
+        feature={"/features/"};
+)
 
 public class RunCukeTest
 {
