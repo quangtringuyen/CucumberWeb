6d9740e783e8cb9af40e1b818fb582b69de7d38c
Tri Nguyen <quangtringuyen@outlook.com>
2016-10-05 21:59:49 +0700

    commit

--- CucumberWeb/src/test/java/com/cucumber/automation/bdt/stepDefinitions/StoreLoginSD.java
+++ CucumberWeb/src/test/java/com/cucumber/automation/bdt/stepDefinitions/StoreLoginSD.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import cucumber.api.java.en.GIVEN;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 public class StoreLoginSD
